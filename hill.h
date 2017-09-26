dfcc0d6234865b94af14309d3ba19e5062924318
GabsGear <gabriel_ghellere@hotmail.com>
2017-09-26 02:41:36 -0300

    Separado algoritmo em uma classe

---
+++ hill.h
@@ -0,0 +1,44 @@
+#ifndef HILLCLIMBING_H_
+#define HILLCLIMBING_H_
+#include <string>
+#include<fstream>
+#include <iostream>
+#include<chrono>
+#include<random>
+#define it 1000
+
+using namespace std;
+
+class HillClimbing {
+public:
+    double getRandom();
+    double cost(double x);
+    double algoritmo();
+};
+
+double HillClimbing::getRandom(){
+    //gerador de numeros aleatorios baseados no tempo (normal desvio padrao 1)
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator (seed);
+    normal_distribution<double> distribution (0.0,1.0);
+    return distribution(generator);
+};
+
+double HillClimbing::cost(double x){
+    return x*x; //funcao x² 
+};
+
+double HillClimbing::algoritmo(){
+    double x = getRandom();
+    double atualCost = cost(x);
+    for(int i = 0; i < it; i++){
+        double tweak = x + getRandom();
+        double newCost = cost(tweak);
+        if(newCost < atualCost){
+            atualCost = newCost;
+            x = tweak;
+        }
+    }
+    return x;
+};
+#endif
--- hill_climbing.cpp
+++ hill_climbing.cpp
@@ -1,7 +1,8 @@
-//gabriel ghellere Hill climbing + aleatorios dist norm
+//gabriel ghellere 
 // para compilar use  g++ hill_climbing.cpp -std=c++11 -o hc
 
 #include"netlist.h"
+#include"hill.h"
 #include<iostream>
 #include<stdlib.h>
 #include<fstream>
@@ -11,36 +12,13 @@
 
 using namespace std;
 
-double getRandom(){
-    //gerador de numeros aleatorios baseados no tempo (normal desvio padrao 1)
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    default_random_engine generator (seed);
-    normal_distribution<double> distribution (0.0,1.0);
-    return distribution(generator);
-}
-
-double cost(double x){
-    return x*x; //funcao x² 
-}
-
-double hillClimbing(){
-    double x = getRandom();
-    double atualCost = cost(x);
-    for(int i = 0; i < it; i++){
-        double tweak = x + getRandom();
-        double newCost = cost(tweak);
-        if(newCost < atualCost){
-            atualCost = newCost;
-            x = tweak;
-        }
-    }
-    return x;
-}
 
 int main(){
     //cout << hillClimbing() << endl;
-     Netlist a;
+     //Netlist a;
     // a.createNtl(5.555);
-    a.runNtl();
+    // a.runNtl();
+    HillClimbing test;
+    cout << test.algoritmo() << endl; 
     return 0;
 }
